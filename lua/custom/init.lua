283e9349d4f8d73c828948eaebd91065a8515509
PhamMinhTan1122 <masterminhtan@gmail.com>
2023-08-14 05:54:07 +0700

    Your commit message

---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+-- shortcut remove comment in nvim
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
